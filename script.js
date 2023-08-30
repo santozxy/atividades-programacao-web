a5e1751a2a7046df18c9f77e580051e1ed968735
Monnuery Júnior <118072648+santozxy@users.noreply.github.com>
2023-08-29 22:04:46 -0300

    Finalizando Tela Principal
    - Adicionando novas Fontes;
    - Criando o Header, Section Home, Projects, Footer  e estilizando-os;
    - Adicionando Favicon;
    - Adicionando Efeito Typing;
    - Criando Objeto JSON para armazenar os projetos de forma estática;
    - Criando função de automatização de exibição de projetos;

---
+++ script.js
@@ -0,0 +1,67 @@
+// ============================ TYPING ANIMATION ============================
+var typed = new Typed(".typing", {
+  strings: [
+    "Olá, seja Bem Vindo!!",
+    "Este projeto é dedicado a disciplina de Programação Web...",
+    "Se sinta a vontade para explorar todos os projetos!!",
+  ],
+  typeSpeed: 60,
+  BackSpeed: 100,
+  loop: false,
+});
+
+const projects = [
+  {
+    id: "1",
+    title: "Atividade 1",
+    image: "assets/test.png",
+    link: "./atividades/atividade-1/index.html",
+  },
+  {
+    id: "2",
+    title: "Atividade 2",
+    image: "assets/test.png",
+    link: "./atividades/atividade-2/index.html",
+  },
+  {
+    id: "3",
+    title: "Atividade 3",
+    image: "assets/test.png",
+    link: "./atividades/atividade-3/index.html",
+  },
+  {
+    id: "4",
+    title: "Atividade 4",
+    image: "./assets/test.png",
+    link: "./atividades/atividade-4/index.html",
+  },
+  {
+    id: "5",
+    title: "Atividade 5",
+    image: "./assets/test.png",
+    link: "./atividades/atividade-5/index.html",
+  },
+  {
+    id: "6",
+    title: "Atividade 6",
+    image: "./assets/test.png",
+    link: "./atividades/atividade-6/index.html",
+  },
+];
+
+const projectContainer = document.getElementById("projects-content");
+
+projects.forEach((projects) => {
+  const projectCard = document.createElement("div");
+  projectCard.innerHTML = `
+  <div class="card">
+    <img src="${projects.image}" alt="${projects.title}" title="Acessar ${projects.title}" loading="lazy" />
+    <div class="links">
+      <a href="${projects.link}" target="_blank">Ver mais
+          <i class="fa-solid fa-arrow-right"></i>
+      </a>
+    </div>
+  </div>
+  `;
+  projectContainer.appendChild(projectCard);
+});
