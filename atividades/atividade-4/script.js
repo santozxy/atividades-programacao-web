885dd5c99f975698479bfb62d1d165025f47b98a
Monnuery Júnior <118072648+santozxy@users.noreply.github.com>
2023-09-05 23:32:12 -0300

    Finalizando Tela Login - Atividade 4
    Adicionando e estruturando as subpastas home e register

---
+++ atividades/atividade-4/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("#form");
+  const emailInput = form.querySelector('input[type="email"]');
+  const passwordInput = form.querySelector('input[type="password"]');
+  const loader = document.querySelector(".loader");
+  const redirects = document.querySelector(".redirects");
+
+  // Adicione um evento "submit" ao formulário
+  form.addEventListener("submit", function (event) {
+    if (emailInput.checkValidity() && passwordInput.checkValidity()) {
+      // Impede o envio padrão do formulário
+      event.preventDefault();
+
+      // Exibe o loader e oculta os redirects
+      loader.style.display = "flex";
+      redirects.style.display = "none";
+
+      setTimeout(function () {
+        loader.style.display = "none";
+        redirects.style.display = "flex";
+        form.submit();
+      }, 3000);
+    }
+  });
+});
